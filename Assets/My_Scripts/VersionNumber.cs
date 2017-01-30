360ad14c01c541f2285aa03247572c73ba56693c
Lucas <lp.peralta1@gmail.com>
2017-01-31 03:27:58 +0800

    corrected version number

--- Assets/My_Scripts/VersionNumber.cs
+++ Assets/My_Scripts/VersionNumber.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public static class VersionNumber {
 
 	//Version number: Major Change - Minor Change - Bug Fix - Branch
-	public static string version = "0.0.1.0";
+	public static string version = "0.3.2.0";
 
 	//Branch Ex. 0 = Master, 100 == quests, 235 = Crafting Weapons, 236 = Crafting Potions
 
+	//0.3 = Quests
+	//0.2 = Experience and Stats and others
+	//0.1 = Movement and Attack
 
 }
